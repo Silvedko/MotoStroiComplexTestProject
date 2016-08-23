833b646321d0f16cfcdfcbce3f2247ad644810c5
oleg.silvestrov <oleg.silvestrov@aofl.com>
2016-08-23 04:39:48 +0300

    Added Main char object

---
+++ Assets/Scripts/Characters/BasicPerson/MainChar/MainChar.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MainChar : BasicPerson 
+{
+	public MainChar (float startHP)
+	{
+		HitPoints = startHP;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void ReduceHitPoints (float hp)
+	{
+		HitPoints -= hp;
+	}
+
+	protected override void OnCollisionEnter (Collision col)
+	{
+		try
+		{
+			BasicBullet bullet = col.gameObject.GetComponent<BasicBullet> ();
+			if(bullet != null)
+			{
+				//ReduceHitPoints (bullet);
+			}
+		}
+		catch (NullReferenceException)
+		{
+			//It's not a bullet
+		}
+	}
+}
